7a15f4f73bc8856aadb92449c61d12267a52467f
Køπs†åπ†iπ <Konstantin.Vankov.Extern@fiduciagad.de>
2021-03-27 17:13:47 +0100

    solution

--- app/src/main/java/com/playground/android101solutions/MainActivity.java
+++ app/src/main/java/com/playground/android101solutions/MainActivity.java
@@ -1,5 +1,6 @@
 package com.playground.android101solutions;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.dialog_title)
+                .setMessage(R.string.dialog_message)
+                .setPositiveButton(R.string.dialog_btn_positive, (dialog, which) -> super.onBackPressed())
+                .setNegativeButton(R.string.dialog_btn_negative, null)
+                .setCancelable(false)
+                .create()
+                .show();
     }
 }
